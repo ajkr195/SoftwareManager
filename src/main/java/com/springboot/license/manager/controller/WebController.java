b3afc08192e31e4ef08e9e3f2e4f3befa04b32dd
Ajay Kumar <Ajay Kumar@DESKTOP-2EDS8Q5>
2024-05-21 15:41:44 -0400

    update

--- src/main/java/com/springboot/license/manager/controller/WebController.java
+++ src/main/java/com/springboot/license/manager/controller/WebController.java
@@ -6,60 +6,59 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class WebController {
 
-	
-	@GetMapping({ "/", "/index" })
+	@GetMapping({ "/index" })
 	String homePage() {
 		return "index";
 	}
-	
+
 	@GetMapping({ "/list" })
 	String listPage() {
 		return "index2";
 	}
-	
+
 	@GetMapping({ "/fetchData" })
 	String fetchDataPage() {
 		return "index3";
 	}
-	
+
 	@GetMapping({ "/fetchAPIData" })
 	String fetchAPIDataPage() {
 		return "index4";
 	}
-	
+
 	@GetMapping({ "/fetchDynamicAPIData" })
 	String fetchDynamicAPIData() {
 		return "index9";
 	}
-	
+
 	@GetMapping({ "/gridjstable" })
 	String gridjstablefetchDynamicAPIData() {
 		return "gridjstable";
 	}
-	
-	@GetMapping({ "/gridjstable_update" })
+
+	@GetMapping({ "/", "/gridjstable_update" })
 	String gridjstable_updatefetchDynamicAPIData() {
 		return "gridjstable_update";
 	}
-	
+
 	@GetMapping({ "/fetchDynamicAPIData2" })
 	String fetchDynamicAPIData2() {
 		return "index9_2";
 	}
-	
+
 	@GetMapping({ "/fetchDynamicAPIData10" })
 	String fetchDynamicAPIData10() {
 		return "index10";
 	}
-	
+
 	@GetMapping({ "/fetchDynamicAPIData102" })
 	String fetchDynamicAPIData102() {
 		return "index10_2";
 	}
+
 	@GetMapping({ "/fetchDynamicAPIData12" })
 	String fetchDynamicAPIData12() {
 		return "index12";
 	}
-	
 
 }
