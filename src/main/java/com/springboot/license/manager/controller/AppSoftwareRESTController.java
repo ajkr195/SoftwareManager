d8023862c853beb6ea8a8fcd23cf30d3ff22234b
Ajay Kumar <Ajay Kumar@DESKTOP-2EDS8Q5>
2024-05-21 23:43:49 -0400

    update

--- src/main/java/com/springboot/license/manager/controller/AppSoftwareRESTController.java
+++ src/main/java/com/springboot/license/manager/controller/AppSoftwareRESTController.java
@@ -17,13 +17,16 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.springboot.license.manager.model.AppSoftware;
 import com.springboot.license.manager.repository.AppSoftwareRepository;
 import com.springboot.license.manager.service.AppSoftwareService;
 
-//@RestController
-//@AllArgsConstructor
+import lombok.AllArgsConstructor;
+
+@RestController
+@AllArgsConstructor
 public class AppSoftwareRESTController {
 
 	private AppSoftwareService appSoftwareService;
--- src/main/java/com/springboot/license/manager/model/AppCustomer.java
+++ src/main/java/com/springboot/license/manager/model/AppCustomer.java
@@ -2,8 +2,11 @@ package com.springboot.license.manager.model;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -29,7 +32,8 @@ public class AppCustomer
     @Column(nullable=false, unique=true)
     private String name;
 
-    @ManyToMany(mappedBy="customers")
+    @JsonIgnore
+    @ManyToMany(mappedBy="customers", fetch = FetchType.LAZY)
     private List<AppSoftware> softwares;
     
 }
